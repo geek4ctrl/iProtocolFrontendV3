e1caf3c38cbc30a29ec12015bcadce0242a1135a
Laurent Mbuyu <laurentm@osiristrading.com>
2024-01-20 16:00:51 +0200

    [New]: Theme implementation has been propagated.

--- app/navigation/page.tsx
+++ app/navigation/page.tsx
@@ -146,7 +146,7 @@ export default async function Index() {
             <section className="py-28" style={{ background: "linear-gradient(152.92deg, rgba(192, 132, 252, 0.2) 4.54%, rgba(232, 121, 249, 0.17) 34.2%, rgba(192, 132, 252, 0.1) 77.55%)", width: "-webkit-fill-available" }}>
                 <div className="max-w-screen-xl mx-auto px-4 md:text-center md:px-8">
                     <div className="max-w-xl space-y-3 md:mx-auto">
-                        <p className="text-gray-800 text-3xl font-semibold sm:text-4xl">
+                        <p>
                             Program of the papal visit
                         </p>
                         <p className="text-gray-600">
